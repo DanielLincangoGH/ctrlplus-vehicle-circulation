9e76b6e6bced4f10baadb33ba0c60922c041172f
Daniel Lincango <flincangoups@gmail.com>
2022-08-12 10:50:24 -0500

    Init ctrl plus vehicle circulation validation project

---
+++ src/main/java/org/conecta/ctrlplus/vehicle/circulation/CirculationApplication.java
@@ -0,0 +1,13 @@
+package org.conecta.ctrlplus.vehicle.circulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CirculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CirculationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/conecta/ctrlplus/vehicle/circulation/CirculationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.conecta.ctrlplus.vehicle.circulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CirculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
