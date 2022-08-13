5922d61274fcaf178da230c29594a3be336bfa25
Daniel Lincango <flincangoups@gmail.com>
2022-08-12 19:30:38 -0500

    Init script for load restriction configs.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,35 @@
+-- MONDAY CONFIG
+INSERT INTO restriction_schedule (id_restriction_schedule, digit, restriction_day, status, create_time ) VALUES (1, '1', '1', 1, CURRENT_TIMESTAMP);
+INSERT INTO schedule_hours (id_schedule_hours, id_restriction_schedule, start_hour, end_hour, status, create_time ) VALUES (1, 1, '06:00', '09:30', 1, CURRENT_TIMESTAMP);
+INSERT INTO schedule_hours (id_schedule_hours, id_restriction_schedule, start_hour, end_hour, status, create_time ) VALUES (2, 1, '18:00', '21:00', 1, CURRENT_TIMESTAMP);
+INSERT INTO restriction_schedule (id_restriction_schedule, digit, restriction_day, status, create_time ) VALUES (2, '2', '1', 1, CURRENT_TIMESTAMP);
+INSERT INTO schedule_hours (id_schedule_hours, id_restriction_schedule, start_hour, end_hour, status, create_time ) VALUES (3, 2, '06:00', '09:30', 1, CURRENT_TIMESTAMP);
+INSERT INTO schedule_hours (id_schedule_hours, id_restriction_schedule, start_hour, end_hour, status, create_time ) VALUES (4, 2, '18:00', '21:00', 1, CURRENT_TIMESTAMP);
+-- TUESDAY CONFIG
+INSERT INTO restriction_schedule (id_restriction_schedule, digit, restriction_day, status, create_time ) VALUES (3, '3', '2', 1, CURRENT_TIMESTAMP);
+INSERT INTO schedule_hours (id_schedule_hours, id_restriction_schedule, start_hour, end_hour, status, create_time ) VALUES (5, 3, '06:00', '09:30', 1, CURRENT_TIMESTAMP);
+INSERT INTO schedule_hours (id_schedule_hours, id_restriction_schedule, start_hour, end_hour, status, create_time ) VALUES (6, 3, '18:00', '21:00', 1, CURRENT_TIMESTAMP);
+INSERT INTO restriction_schedule (id_restriction_schedule, digit, restriction_day, status, create_time ) VALUES (4, '4', '2', 1, CURRENT_TIMESTAMP);
+INSERT INTO schedule_hours (id_schedule_hours, id_restriction_schedule, start_hour, end_hour, status, create_time ) VALUES (7, 4, '06:00', '09:30', 1, CURRENT_TIMESTAMP);
+INSERT INTO schedule_hours (id_schedule_hours, id_restriction_schedule, start_hour, end_hour, status, create_time ) VALUES (8, 4, '18:00', '21:00', 1, CURRENT_TIMESTAMP);
+-- WEDNESDAY CONFIG
+INSERT INTO restriction_schedule (id_restriction_schedule, digit, restriction_day, status, create_time ) VALUES (5, '5', '3', 1, CURRENT_TIMESTAMP);
+INSERT INTO schedule_hours (id_schedule_hours, id_restriction_schedule, start_hour, end_hour, status, create_time ) VALUES (9, 5, '06:00', '09:30', 1, CURRENT_TIMESTAMP);
+INSERT INTO schedule_hours (id_schedule_hours, id_restriction_schedule, start_hour, end_hour, status, create_time ) VALUES (10, 5, '18:00', '21:00', 1, CURRENT_TIMESTAMP);
+INSERT INTO restriction_schedule (id_restriction_schedule, digit, restriction_day, status, create_time ) VALUES (6, '6', '3', 1, CURRENT_TIMESTAMP);
+INSERT INTO schedule_hours (id_schedule_hours, id_restriction_schedule, start_hour, end_hour, status, create_time ) VALUES (11, 6, '06:00', '09:30', 1, CURRENT_TIMESTAMP);
+INSERT INTO schedule_hours (id_schedule_hours, id_restriction_schedule, start_hour, end_hour, status, create_time ) VALUES (12, 6, '18:00', '21:00', 1, CURRENT_TIMESTAMP);
+-- THURSDAY CONFIG
+INSERT INTO restriction_schedule (id_restriction_schedule, digit, restriction_day, status, create_time ) VALUES (7, '7', '4', 1, CURRENT_TIMESTAMP);
+INSERT INTO schedule_hours (id_schedule_hours, id_restriction_schedule, start_hour, end_hour, status, create_time ) VALUES (13, 7, '06:00', '09:30', 1, CURRENT_TIMESTAMP);
+INSERT INTO schedule_hours (id_schedule_hours, id_restriction_schedule, start_hour, end_hour, status, create_time ) VALUES (14, 7, '18:00', '21:00', 1, CURRENT_TIMESTAMP);
+INSERT INTO restriction_schedule (id_restriction_schedule, digit, restriction_day, status, create_time ) VALUES (8, '8', '4', 1, CURRENT_TIMESTAMP);
+INSERT INTO schedule_hours (id_schedule_hours, id_restriction_schedule, start_hour, end_hour, status, create_time ) VALUES (15, 8, '06:00', '09:30', 1, CURRENT_TIMESTAMP);
+INSERT INTO schedule_hours (id_schedule_hours, id_restriction_schedule, start_hour, end_hour, status, create_time ) VALUES (16, 8, '18:00', '21:00', 1, CURRENT_TIMESTAMP);
+-- FRIDAY CONFIG
+INSERT INTO restriction_schedule (id_restriction_schedule, digit, restriction_day, status, create_time ) VALUES (9, '9', '5', 1, CURRENT_TIMESTAMP);
+INSERT INTO schedule_hours (id_schedule_hours, id_restriction_schedule, start_hour, end_hour, status, create_time ) VALUES (17, 9, '06:00', '09:30', 1, CURRENT_TIMESTAMP);
+INSERT INTO schedule_hours (id_schedule_hours, id_restriction_schedule, start_hour, end_hour, status, create_time ) VALUES (18, 9, '18:00', '21:00', 1, CURRENT_TIMESTAMP);
+INSERT INTO restriction_schedule (id_restriction_schedule, digit, restriction_day, status, create_time ) VALUES (10, '0', '5', 1, CURRENT_TIMESTAMP);
+INSERT INTO schedule_hours (id_schedule_hours, id_restriction_schedule, start_hour, end_hour, status, create_time ) VALUES (19, 10, '06:00', '09:30', 1, CURRENT_TIMESTAMP);
+INSERT INTO schedule_hours (id_schedule_hours, id_restriction_schedule, start_hour, end_hour, status, create_time ) VALUES (20, 10, '18:00', '21:00', 1, CURRENT_TIMESTAMP);
